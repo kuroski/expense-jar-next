3a8e47bb5a338bcb92a3d91dd2c15e4719088a42
Daniel Kuroski <daniel.kuroski@gmail.com>
2021-02-05 08:45:28 +0100

    Continue subscriptions form

--- pages/subscriptions/new.tsx
+++ pages/subscriptions/new.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import { Button, FormControl, FormErrorMessage, FormLabel, Input, Select, useTheme } from '@chakra-ui/react'
-import { Field, Form, Formik } from 'formik'
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Stack,
+} from '@chakra-ui/react'
+import { Field, FieldProps, Form, Formik, useFormik } from 'formik'
 import * as Yup from 'yup'
 
 const SubscriptionSchema = Yup.object().shape({
-  // color: Yup.string().required("The 'color' field is required"),
-  // cycleAmount: Yup.number().required("The 'cycle amount' field is required"),
+  color: Yup.string().required("The 'color' field is required"),
+  cycleAmount: Yup.number().required("The 'cycle amount' field is required"),
   // cyclePeriod: Yup.string().required("The 'cycle period' field is required"),
   // icon: Yup.string().required("The 'icon' field is required"),
   name: Yup.string().required("The 'name' field is required"),
@@ -14,19 +27,20 @@ const SubscriptionSchema = Yup.object().shape({
   // firstBill: Yup.date().required("The 'date' field is required"),
 })
 
-type Subscription = {
+type FormValues = {
   name: string
+  color: string
+  cycleAmount: number
 }
 
 const NewSubscription = () => {
-  // const [name, setName] = useState('')
   const router = useRouter()
-  const theme = useTheme()
 
-  const saveSubscription = async (name = '') => {
+  const saveSubscription = async (values: FormValues) => {
+    console.log(values)
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/api/subscription/`, {
       method: 'POST',
-      body: JSON.stringify({ name }),
+      body: JSON.stringify(values),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -37,63 +51,66 @@ const NewSubscription = () => {
     router.push('/')
   }
 
-  const initialValues: Subscription = { name: '' }
+  const form = useFormik({
+    initialValues: {
+      name: '',
+      color: '#000000',
+      cycleAmount: 1,
+    },
+    validationSchema: SubscriptionSchema,
+    onSubmit: (values, actions) => {
+      console.log({ values, actions })
+      saveSubscription(values)
+    },
+  })
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={SubscriptionSchema}
-      onSubmit={(values, actions) => {
-        console.log({ values, actions })
-        saveSubscription(values.name)
-      }}
-    >
-      {(props) => (
-        <Form>
-          <Field name="name">
-            {({ field, form }: any) => (
-              <FormControl isInvalid={form.errors.name && form.touched.name}>
-                <FormLabel htmlFor="name">Name</FormLabel>
-                <Input {...field} id="name" placeholder="name" />
-                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-              </FormControl>
-            )}
-          </Field>
+    <form onSubmit={form.handleSubmit}>
+      <Stack spacing={4}>
+        <FormControl id="name" isInvalid={Boolean(form.errors.name && form.touched.name)}>
+          <FormLabel>Name</FormLabel>
+          <Input type="text" onChange={form.handleChange} value={form.values.name} />
+          <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+        </FormControl>
 
-          <Field name="color">
-            {({ field, form }: any) => (
-              <FormControl isInvalid={form.errors.color && form.touched.color}>
-                <FormLabel htmlFor="color">color</FormLabel>
-                <Select {...field} id="color" placeholder="color">
-                  {Object.keys(theme.colors).map((color) => (
-                    <option key={color} value={color}>
-                      {color}
-                    </option>
-                  ))}
-                </Select>
-                <FormErrorMessage>{form.errors.color}</FormErrorMessage>
-              </FormControl>
-            )}
-          </Field>
+        <FormControl id="color" isInvalid={Boolean(form.errors.color && form.touched.color)}>
+          <FormLabel>color</FormLabel>
+          <Input variant="flushed" type="color" onChange={form.handleChange} value={form.values.color} />
+          <FormErrorMessage>{form.errors.color}</FormErrorMessage>
+        </FormControl>
 
-          <pre>{JSON.stringify(props.errors)}</pre>
-          <Button mt={4} colorScheme="teal" isLoading={props.isSubmitting} type="submit">
-            Submit
-          </Button>
-        </Form>
-      )}
-    </Formik>
+        <FormControl id="cycleAmount" isInvalid={Boolean(form.errors.cycleAmount && form.touched.cycleAmount)}>
+          <FormLabel>cycleAmount</FormLabel>
+          <NumberInput
+            min={1}
+            max={31}
+            onChange={(value) =>
+              form.handleChange({
+                target: {
+                  id: 'cycleAmount',
+                  value,
+                },
+              })
+            }
+            value={form.values.cycleAmount}
+            allowMouseWheel
+            inputMode="numeric"
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+          <FormErrorMessage>{form.errors.cycleAmount}</FormErrorMessage>
+        </FormControl>
 
-    // <div>
-    //   <Input
-    //     name="text-input-name"
-    //     placeholder="Text input placeholder..."
-    //     value={name}
-    //     onChange={(e) => setName(e.target.value)}
-    //   />
-
-    //   <Button onClick={saveSubscription}>Save</Button>
-    // </div>
+        <pre>{JSON.stringify(form.errors)}</pre>
+        <Button mt={4} colorScheme="teal" isLoading={form.isSubmitting} type="submit">
+          Submit
+        </Button>
+      </Stack>
+    </form>
   )
 }
 
