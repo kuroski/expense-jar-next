83fbf2c49ee61fa11b45e32746f5c22034e05168
Daniel Kuroski <daniel.kuroski@gmail.com>
2021-03-07 09:50:55 +0100

    Remove uneeded path for useDebounce

--- components/iconSelect.tsx
+++ components/iconSelect.tsx
@@ -14,7 +14,7 @@ import {
 } from '@chakra-ui/react'
 import * as materialIcons from 'react-icons/md'
 import { flow } from 'fp-ts/lib/function'
-import { useDebounce } from 'use-debounce/lib'
+import { useDebounce } from 'use-debounce'
 
 type Props = {
   value: string
