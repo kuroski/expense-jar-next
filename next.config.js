7930a60a6cbeab38b5281aba7e2e7b1c8e54792f
Daniel Kuroski <daniel.kuroski@gmail.com>
2021-03-24 08:28:32 +0100

    Disable PWA in development

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   pwa: {
+    disable: process.env.NODE_ENV === 'development',
     dest: 'public',
     runtimeCaching,
   },
