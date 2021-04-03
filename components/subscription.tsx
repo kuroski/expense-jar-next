6d34252e829a1906565b8c495b61d3e73c4ae3c5
Daniel Kuroski <daniel.kuroski@gmail.com>
2021-04-03 15:06:22 +0200

    Add billing date calculation to a hook

--- components/subscription.tsx
+++ components/subscription.tsx
@@ -3,118 +3,17 @@ import React from 'react'
 import { CgMail } from 'react-icons/cg'
 import type * as types from '@/framework/subscriptions/types'
 import * as simpleIcons from 'react-icons/si'
-import * as fns from 'date-fns/fp'
-import { not, pipe } from 'fp-ts/lib/function'
-import * as O from 'fp-ts/lib/Option'
 import { DeleteIcon } from '@chakra-ui/icons'
+import useNextBilling from '@/framework/subscriptions/useNextBilling'
 
 type SubscriptionItemProps = types.Subscription & {
   onDelete: (id: string) => void
 }
 
-const now = new Date()
-const setCurrentYear = fns.setYear(fns.getYear(now))
-const setCurrentMonth = fns.setMonth(fns.getMonth(now))
-const setCurrentWeek = fns.setWeek(fns.getWeek(now))
-const setCurrentDay = fns.setDay(fns.getDay(now))
-const isEqualNow = fns.isEqual(now)
-const isBeforeNow = fns.isBefore(now)
-
 const SubscriptionItem = (item: SubscriptionItemProps): JSX.Element => {
   const [, icon] = Object.entries(simpleIcons).find(([key]) => key === item.icon) || []
   const price = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(item.price)
-
-  const currentBilling = pipe(item.firstBill, (firstBill: Date) => {
-    switch (item.cyclePeriod) {
-      case 'day': {
-        const isNextBillToday = (mod: boolean) => (firstBill: Date): boolean =>
-          !mod && pipe(firstBill, setCurrentYear, setCurrentMonth, setCurrentDay, isEqualNow)
-        const differenceInDays = fns.differenceInDays(firstBill, now)
-        const mod = differenceInDays % item.cycleAmount
-
-        return pipe(
-          firstBill,
-          O.fromPredicate(pipe(Boolean(mod), isNextBillToday, not)),
-          O.map(fns.add({ days: differenceInDays + (item.cycleAmount - mod) })),
-          O.chain((nextBill) =>
-            pipe(
-              nextBill,
-              O.fromPredicate(isBeforeNow),
-              O.map(fns.addDays(item.cycleAmount)),
-              O.altW<Date>(() => O.some(nextBill)),
-            ),
-          ),
-          O.getOrElse(() => now),
-        )
-      }
-
-      case 'week': {
-        const isNextBillToday = (mod: boolean) => (firstBill: Date): boolean =>
-          !mod && pipe(firstBill, setCurrentYear, setCurrentWeek, isEqualNow)
-        const differenceInWeeks = fns.differenceInWeeks(firstBill, now)
-        const mod = differenceInWeeks % item.cycleAmount
-
-        return pipe(
-          firstBill,
-          O.fromPredicate(pipe(Boolean(mod), isNextBillToday, not)),
-          O.map(fns.add({ weeks: differenceInWeeks + (item.cycleAmount - mod) })),
-          O.chain((nextBill) =>
-            pipe(
-              nextBill,
-              O.fromPredicate(isBeforeNow),
-              O.map(fns.addWeeks(item.cycleAmount)),
-              O.altW<Date>(() => O.some(nextBill)),
-            ),
-          ),
-          O.getOrElse(() => now),
-        )
-      }
-
-      case 'month': {
-        const isNextBillToday = (mod: boolean) => (firstBill: Date): boolean =>
-          !mod && pipe(firstBill, setCurrentYear, setCurrentMonth, isEqualNow)
-        const differenceInMonths = fns.differenceInMonths(firstBill, now)
-        const mod = differenceInMonths % item.cycleAmount
-
-        return pipe(
-          firstBill,
-          O.fromPredicate(pipe(Boolean(mod), isNextBillToday, not)),
-          O.map(fns.add({ months: differenceInMonths + (item.cycleAmount - mod) })),
-          O.chain((nextBill) =>
-            pipe(
-              nextBill,
-              O.fromPredicate(isBeforeNow),
-              O.map(fns.addMonths(item.cycleAmount)),
-              O.altW<Date>(() => O.some(nextBill)),
-            ),
-          ),
-          O.getOrElse(() => now),
-        )
-      }
-
-      case 'year': {
-        const isNextBillToday = (mod: boolean) => (firstBill: Date): boolean =>
-          !mod && pipe(firstBill, setCurrentYear, isEqualNow)
-        const differenceInYears = fns.differenceInYears(firstBill, now)
-        const mod = differenceInYears % item.cycleAmount
-
-        return pipe(
-          firstBill,
-          O.fromPredicate(pipe(Boolean(mod), isNextBillToday, not)),
-          O.map(fns.add({ years: differenceInYears + (item.cycleAmount - mod) })),
-          O.chain((nextBill) =>
-            pipe(
-              nextBill,
-              O.fromPredicate(isBeforeNow),
-              O.map(fns.addYears(item.cycleAmount)),
-              O.altW<Date>(() => O.some(nextBill)),
-            ),
-          ),
-          O.getOrElse(() => now),
-        )
-      }
-    }
-  })
+  const { formattedFirstBilling, formattedCurrentBilling, timeUntilNextBilling } = useNextBilling(item)
 
   return (
     <Box p={4} shadow="md" borderWidth="1px" rounded="lg" textAlign={['center', 'left']} direction="column">
@@ -132,9 +31,9 @@ const SubscriptionItem = (item: SubscriptionItemProps): JSX.Element => {
           </span>
         </StatLabel>
         <StatNumber>{price}</StatNumber>
-        <StatHelpText>First: {fns.format('PP', item.firstBill)}</StatHelpText>
-        <StatHelpText>Next: {fns.format('PP', currentBilling)}</StatHelpText>
-        <StatHelpText>in {fns.formatDistance(currentBilling, now)}</StatHelpText>
+        <StatHelpText>First: {formattedFirstBilling}</StatHelpText>
+        <StatHelpText>Next: {formattedCurrentBilling}</StatHelpText>
+        <StatHelpText>in {timeUntilNextBilling}</StatHelpText>
         <IconButton
           aria-label={`Delete subscription: ${item.name}`}
           colorScheme="red"
